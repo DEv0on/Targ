0c6ae4970b16103cfc4498201799d525567914ac
DEv0on <bananamen02@gmail.com>
2020-06-23 17:36:42 +0200

    Created AE2Utils and settings

---
+++ 0/utils/AE2Utils.lua
@@ -0,0 +1,27 @@
+--
+-- Created by DEv0on
+-- Date: 23.06.2020
+-- Time: 17:24
+--
+
+AE2Utils = {}
+
+function AE2Utils:getFingerprint(id,dmg,nbt)
+    if nbt == "" then
+        return {id= id, dmg= tonumber(dmg)}
+    else
+        return {id = id, dmg = tonumber(dmg), nbt_hash = nbt}
+    end
+end
+
+function AE2Utils:getItemQty(fingerprint,AE2)
+    itemList = AE2.getAvailableItems()
+    for k,v in pairs(itemList) do
+        if v.fingerprint.id == fingerprint.id and v.fingerprint.dmg == fingerprint.dmg and v.fingerprint.nbt_hash == fingerprint.nbt_hash then
+            return v.size
+        end
+    end
+    return 0
+end
+
+return AE2Utils
